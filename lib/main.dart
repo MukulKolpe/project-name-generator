d5ef1741ad98124f26598ead3e3bed333881c200
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-02-21 17:20:42 +0530

    testing pre commit hook

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Generator',
+      title: 'Startup Generator--',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.white,
